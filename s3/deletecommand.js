eca4fe96ad8aca4c51285fe2ba7a29636cc0dc3c
Rahul Bagchi <rahulstech18@gmail.com>
2025-08-13 09:42:46 +0530

    delete multiple s3 objects

--- s3/deletecommand.js
+++ s3/deletecommand.js
@@ -1,10 +1,14 @@
-const { DeleteObjectCommand } = require('@aws-sdk/client-s3');
-const { s3Client, BUCKET_NAME } = require('./s3client');
+const { DeleteObjectCommand, DeleteObjectsCommand } = require('@aws-sdk/client-s3');
+const { s3Client } = require('./s3client');
+const s3client = require('./s3client');
 
+const BUCKET_NAME = "aws-lessons-nodejs"
+
+// delete single object 
 async function deleteObject(Key) {
 	const cmd = new DeleteObjectCommand({
 		Bucket: BUCKET_NAME,
-		Key,
+		Key, // key must points to an exact object not a folder, otherwise nothing will be delete but 204 will be returned
 	});
 
 	/**
@@ -26,8 +30,27 @@ async function deleteObject(Key) {
 	console.log(res);
 }
 
+// delete multiple objects
+async function deleteMultipleObjects(keys) {
+	const cmd = new DeleteObjectsCommand({
+		Bucket: BUCKET_NAME, 
+		Delete: {
+			Objects: keys.map( Key => ({ Key })) // here also the key must be exact
+		}
+	})
+
+	const res = await s3Client.send(cmd);
+	console.log(res);
+}
+
 // delete existing object; upload the object first in the s3 bucket manually
-deleteObject('image.png');
+deleteMultipleObjects(
+	[
+		"medias/GUEST/1755053892875/7ef357fa-acb6-415f-aa3a-c5af714db956",
+		"medias/GUEST/1755053892875/cbb50697-fd6b-4c69-8a87-3f730ff791c1",
+		"medias/GUEST/1755053892875/" // delete the directory also
+	]
+);
 
 // delete non-existing object
-deleteObject('non-existing.jpg');
+// deleteObject('non-existing.jpg');
